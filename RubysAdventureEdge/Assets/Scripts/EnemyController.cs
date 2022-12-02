9e08e58ec3bef82f81231a24decf3556f5424d91
NEDGE3 <739516@student.dusd.net>
2022-12-02 09:54:57 -0800

    Animations
    Started animating the Robot and Ruby

--- RubysAdventureEdge/Assets/Scripts/EnemyController.cs
+++ RubysAdventureEdge/Assets/Scripts/EnemyController.cs
@@ -4,18 +4,22 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
-    public float speed = 3.0f;
+    public float speed;
     public bool vertical;
-    public float changeTime = 3.0f
+    public float changeTime = 3.0f;
 
     Rigidbody2D rigidbody2D;
     float timer;
     int direction = 1;
 
+    Animator animator;
+
     // Start is called before the first frame update
     void Start()
     {
         rigidbody2D = GetComponent<Rigidbody2D>();
+        timer = changeTime;
+        animator = GetComponent<Animator>();
     }
 
     void FixedUpdate()
@@ -24,17 +28,39 @@ public class EnemyController : MonoBehaviour
 
         if (vertical)
         {
-            position.y = rigidbody2D.position;
+            position.y = position.y + Time.deltaTime * speed * direction;
+            animator.SetFloat("Move X", 0);
+            animator.SetFloat("Move Y", direction);
         }
         else
         {
-            position.x = position.x + Time.deltaTime * speed;
+            position.x = position.x + Time.deltaTime * speed * direction;
+            animator.SetFloat("Move X", direction);
+            animator.SetFloat("Move Y", 0);
         }
-        rigidbody2D.MovePosition(position)
+        rigidbody2D.MovePosition(position);
     }
     // Update is called once per frame
     void Update()
     {
-        
+        timer -= Time.deltaTime;
+
+        if (timer < 0)
+        {
+            direction = -direction;
+            timer = changeTime;
+        }
+       
+    }
+
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        RubyController player = other.gameObject.GetComponent<RubyController>();
+
+        if (player != null)
+        {
+            player.ChangeHealth(-1);
+        }
     }
 }
